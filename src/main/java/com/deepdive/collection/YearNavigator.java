2e41cee48ebc391cc76a54b173f7c0ebcef9fd90
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-10-14 21:08:55 +0300

    feat:

--- src/main/java/com/deepdive/collection/YearNavigator.java
+++ src/main/java/com/deepdive/collection/YearNavigator.java
@@ -1,26 +1,35 @@
 package com.deepdive.collection;
 
 import com.deepdive.model.MediaContent;
+import com.deepdive.model.enums.MediaType;
 
+import java.util.Comparator;
 import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class YearNavigator {
     private NavigableSet<MediaContent> contentByYear;
 
-    public YearNavigator(MediaCatalog catalog) {
-        this.contentByYear = contentByYear;
+    private static class DummyContent extends MediaContent {
+        public DummyContent(int year) {
+            super("", year, MediaType.DOCUMENTARY);
+        }
     }
-// TODO: Создай конструктор
-    // - Принимает MediaCatalog
-    // - Создает TreeSet с Comparator по годам
-    // - Добавляет весь контент из каталога
 
-
-
-
-
-
-
+    public YearNavigator(MediaCatalog catalog) {
+        Comparator<MediaContent> byYear = new Comparator<MediaContent>() {
+            @Override
+            public int compare(MediaContent o1, MediaContent o2) {
+                int yearCompare = Integer.compare(o1.getYear(), o2.getYear());
+                if (yearCompare == 0 && !o1.getTitle().isEmpty() && o2.getTitle().isEmpty()) {
+                    return o1.getTitle().compareTo(o2.getTitle());
+                }
+                return yearCompare;
+            }
+        };
+        this.contentByYear = new TreeSet<>(byYear);
+        contentByYear.addAll(catalog.getAllMedia());
+    }
 
 
     // TODO: Создай метод getContentBetweenYears
@@ -30,35 +39,16 @@ public class YearNavigator {
     // - Использует subSet с фиктивными объектами
 
 
-
-
-
-
-
-
-
-    // TODO: Создай метод getContentBefore
-    // - Принимает year (не включительно)
-    // - Возвращает NavigableSet<MediaContent>
-    // - Создай фиктивный объект для границы
-    // - Использует headSet
-
-
-
-
-
-
+    public NavigableSet<MediaContent> getContentBefore(int year) {
+        MediaContent dummy = new DummyContent(year);
+        return contentByYear.tailSet(dummy, true);
+    }
 
     // TODO: Создай метод getContentAfter
     // - Принимает year (включительно)
     // - Возвращает NavigableSet<MediaContent>
     // - Создай фиктивный объект для границы
     // - Использует tailSet
-
-
-
-
-
 
 
     // TODO: Создай метод getYearRange
